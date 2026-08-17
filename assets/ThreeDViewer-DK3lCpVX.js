f3bcf9a45bf1d8018de301fe6f0c64e4e87a31a4
Jeffersonqu <jeffersonqu0428@gmail.com>
2026-08-16 21:32:29 -0230

    Updates

--- assets/ThreeDViewer-DK3lCpVX.js
+++ assets/ThreeDViewer-DK3lCpVX.js
@@ -1,4 +1,4 @@
-import{r as Ye,a as Yy,g as qy,j as nt,L as jy}from"./index-CykLQkEl.js";/**
+import{r as Ye,a as Yy,g as qy,j as nt,L as jy}from"./index-mHA3qSdJ.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
